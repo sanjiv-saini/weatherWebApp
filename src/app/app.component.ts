9af8d8f24c665299dd7b157765ef667cbd10501a
sanju singh <sanju_saini@outlook.com>
2018-03-15 00:15:51 +0530

    Add skeleton for weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Sunshine';
+  private country = 'US';
+  private city = 'London';
+  private selected = {
+                        main:"Clear",
+                        description: "clear sky",
+                        temp: 2.79,
+                        temp_min: 2.1,
+                        temp_max: 2.79,
+                        day: "Today",
+                        pressure: 323,
+                        humidity: 32,
+                        wind: 101
+                      }
+  private forecasts = [
+                        {
+                          main:"Clear",
+                          description: "clear sky",
+                          temp: 2.79,
+                          temp_min: 2.1,
+                          temp_max: 2.79,
+                          day: "Today"
+                        },
+                        {
+                          main:"Clear",
+                          description: "clear sky",
+                          temp: 2.79,
+                          temp_min: 2.1,
+                          temp_max: 2.79,
+                          day: "Tomorrow"
+                        },
+                        {
+                          main:"Clear",
+                          description: "clear sky",
+                          temp: 2.79,
+                          temp_min: 2.1,
+                          temp_max: 2.79,
+                          day:"Sunday"
+                        },
+                        {
+                          main:"Clear",
+                          description: "clear sky",
+                          temp: 2.79,
+                          temp_min: 2.1,
+                          temp_max: 2.79,
+                          day: "Monday"
+                        }
+                      ]
+
 }
