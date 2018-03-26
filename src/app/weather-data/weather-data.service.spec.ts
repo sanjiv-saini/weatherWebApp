06c34ba75f72b100c3e0dafac758d7e2293bf2d1
sanju singh <sanju_saini@outlook.com>
2018-03-26 10:05:51 +0530

    Add unit test for day calculation in data service

--- src/app/weather-data/weather-data.service.spec.ts
+++ src/app/weather-data/weather-data.service.spec.ts
@@ -2,10 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { WeatherDataService } from './weather-data.service';
 import { environment } from '../../environments/environment';
-import { parsedJson, responseJson } from './mockData';
 import { HttpClientModule } from '@angular/common/http';
-
-
 
 describe('WeatherDataService', () => {
 
@@ -28,8 +25,19 @@ describe('WeatherDataService', () => {
     }
   }));
 
-  it('should return properly parsed data for given response data', inject([WeatherDataService], (service: WeatherDataService) => {
-    expect(service.parseResponse(responseJson)).toEqual(parsedJson);
+  it('should give correct day for given time in seconds', inject([WeatherDataService], (service: WeatherDataService) => {
+  const today = new Date();
+    expect(service.getDay(today.getTime() / 1000)).toEqual('Today');
+
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  expect(service.getDay(tomorrow.getTime() / 1000)).toEqual('Tomorrow');
+
+  for (let i = 2; i < 7; i++) {
+    const nextDay = new Date();
+    nextDay.setDate(today.getDate() + i);
+    expect(service.getDay(nextDay.getTime() / 1000)).toEqual(service.getDayInString(nextDay.getDay()));
+  }
   }));
 
 });
